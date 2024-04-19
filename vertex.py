2afe15419e8d322f2d6e8c714c135e78309a647f
IsraCard <isracard66@gmail.com>
2024-04-19 16:25:14 -0600

    Changes to be committed: 	modified:   vertex.py
        Implementada la api para llamar la IA

--- vertex.py
+++ vertex.py
@@ -1,31 +1,18 @@
 import vertexai
-from vertexai.language_models import TextGenerationModel
+from vertexai.language_models import CodeGenerationModel
 
-
-def interview(
-    temperature: float,
-    project_id: str, # JSON implementation, needs to 
-    location = "us-south1"
-    
-) -> str:
-    """Ideation example with a Large Language Model"""
-
-    vertexai.init(project=project_id, location=location)
-    # TODO developer - override these parameters as needed:
+def interview(prompt:str,ProjectID:str):
+    vertexai.init(project=ProjectID, location="us-central1")
     parameters = {
-        "temperature": temperature,  # Temperature controls the degree of randomness in token selection. 0-1
-        "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
-        "top_p": 0.8,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
-        "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
+    "candidate_count": 1,
+    "max_output_tokens": 100,
+    "temperature": 0.2
     }
-
-    model = TextGenerationModel.from_pretrained("text-bison")
+    model = CodeGenerationModel.from_pretrained("code-bison")
     response = model.predict(
-        "Dime 2 cosas sobre los pinguinos",
-        **parameters,
-    )
-    print(f"Response from Model: {response.text}") #quitar al iniciar el test
+        prefix = """dime 2 cosas sobre pinguinos""",
+        **parameters
+    )   
+    print(f"Response from Model: {response.text}")
 
-    return response.text
-
-interview(0,"")#ProjectID
+interview("","") #El projectID va como SEGUNDO argumento de la funcion
