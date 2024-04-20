1621a3cce5f07e984d9ef046543b0d512fee4888
IsraCard <isracard66@gmail.com>
2024-04-19 18:29:27 -0600

    Changes to be committed: 	modified:   vertex.py
    Fixed vertex to not store api keys, fixed typo in Api_Keys
    vertex now extracts the key from Api_Keys as Google_Vertex

--- vertex.py
+++ vertex.py
@@ -1,18 +1,30 @@
-import vertexai
+import vertexai, json
 from vertexai.language_models import CodeGenerationModel
 
-def interview(prompt:str,ProjectID:str):
+# Load API keys from JSON file
+def GetKey():
+    with open('keys/API_Keys.json', 'r') as keys_file:
+        keys = json.load(keys_file)
+        # Check if the Geocoding key is missing
+        if "Google_Vertex" not in keys or not keys["Google_Vertex"]:
+            print('Missing Google cloud project id')
+    ProjectID = keys["Google_Vertex"]
+    print(ProjectID)
+    return ProjectID
+
+#Call Vertex AI
+def interview(prompt:str): #Prompt must be specific and clear
+    ProjectID = GetKey()
     vertexai.init(project=ProjectID, location="us-central1")
     parameters = {
-    "candidate_count": 1,
-    "max_output_tokens": 100,
-    "temperature": 0.2
+    "candidate_count": 1, #Amount of responses
+    "max_output_tokens": 100, #Max length of response
+    "temperature": 0.2 #Randomness of result. 0 means less random, 1 means more random and creative
     }
+    #wake up the model and prompt it
     model = CodeGenerationModel.from_pretrained("code-bison")
     response = model.predict(
-        prefix = """dime 2 cosas sobre pinguinos""",
+        prefix = prompt,
         **parameters
     )   
-    print(f"Response from Model: {response.text}")
-
-interview("","") #El projectID va como SEGUNDO argumento de la funcion
+    return response.text #Returns a String
